2c8cb2197ec7178cc63e634ca6a27969b51abd4e
Seunggu0305 <seunggu_35@naver.com>
2020-07-25 20:55:42 +0900

    map&vector

--- boj/silver/1620.cpp
+++ boj/silver/1620.cpp
@@ -2,57 +2,33 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <map>
 
 using namespace std;
 
-int main(void)
+int main()
 {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-
-	int N, M;
-	cin >> N >> M;
-	vector<string> v(N + 1);
-	
-	for (int i = 1; i <= N; i++)
-		cin >> v[i];
-	vector<pair<string, int>> sortV(N);
-	
-	for (int i = 0; i < N; i++) {
-		sortV[i].first = v[i + 1];
-		sortV[i].second = i + 1;
+	ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL);
+	int  n,m;
+	cin >> n >> m;
+	vector<string> pocketmon;
+	map<string, int> pockmap;
+	for(int i=1;i<=n;i++) {
+		string name;
+		cin >> name;
+		pockmap.insert({name, i});
+		pocketmon.push_back(name);
 	}
-	
-	sort(sortV.begin(), sortV.end());
-	
-	for (int i = 0; i < M; i++)	{
-		string s;
-		cin >> s;
-		if (s[0] >= '0' && s[0] <= '9')	{
-			int idx = 0;
-			for (int j = 0; j < s.length(); j++) {
-				idx += s[j] - '0';
-				idx *= 10;
-			}
-			idx /= 10;
-			cout << v[idx] << "\n";
+	for(int i=0;i<m;i++) {
+		string req;
+		cin >> req;
+		if(isdigit(req[0])) {
+			int idx = stoi(req) - 1;
+			cout << pocketmon[idx] << '\n';
 		}
 		else {
-			int low = 0, high = N - 1;
-			while (low <= high)	{
-				int mid = (low + high) / 2;
-				if (sortV[mid].first == s) {
-					cout << sortV[mid].second << "\n";
-					break;
-				}
-				else if (sortV[mid].first < s)
-					low = mid + 1;
-				else
-					high = mid - 1;
-			}
+			cout << pockmap[req] << '\n';
 		}
 	}
 	return 0;
 }
-
-
